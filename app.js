cdcf1b794f6193833563d123f80f5ce8622d05b3
abdillahtop <abdillahtop@gmail.com>
2019-09-05 11:47:02 +0700

    block cors event post menu

--- app.js
+++ app.js
@@ -10,16 +10,33 @@ const menuRoute = require('./src/routers/menu')
 const catRoute = require('./src/routers/category')
 const userRoute = require('./src/routers/user')
 const histoRoute = require('./src/routers/history')
+const whitelist = process.env.WHITELIST
+
+const corsOptions = (req, callback) => {
+    if (whitelist.split(',').indexOf(req.header('Origin')) !== -1) {
+        console.log('Success')
+        return callback(null, {
+            origin: true
+        })
+    } else {
+        console.log('Failed')
+        return callback(null, {
+            origin: false
+        })
+    }
+}
 
 app.listen(port, () => {
     console.log(`\n App listening on port ${port} \n`)
 })
 
 app.use(cors())
+app.options('*', cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+app.use('/uploads', express.static('uploads'));
 
 app.use('/menu', menuRoute)
 app.use('/category', catRoute)
